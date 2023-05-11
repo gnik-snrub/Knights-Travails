e9775396d20546df42cb80fcf480bbd322185b67
Josiah Morris <josiahbmorris@hotmail.com>
2023-05-12 03:35:07 +0900

    Added node factory function, to be used in graph

--- knights_travails.js
+++ knights_travails.js
@@ -1,3 +1,17 @@
+const node = (x, y) => {
+  const adjacents = []
+
+  const addAdjacent = (node) => {
+    adjacents.push(node.value)
+  }
+
+  return {
+    value: [x, y],
+    adjacents,
+    addAdjacent,
+  }
+}
+
 const graph = () => {
 
 }
