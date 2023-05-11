8164fb02dd29e08ccc7e050338d06a0521f8af22
Josiah Morris <josiahbmorris@hotmail.com>
2023-05-12 03:39:31 +0900

    Added buildGraph function, which generates a chessboard. Nodes are adjacent based on knight movement

--- knights_travails.js
+++ knights_travails.js
@@ -20,4 +20,26 @@ const graph = () => {
     return vertices[index]
   }
 
+  const buildGraph = () => {
+    const boardSize = 8
+    for (let x = 0; x < boardSize; x++) {
+      for (let y = 0; y <= boardSize; y++) {
+        const newNode = node(x, y)
+        vertices.push(newNode)
+      }
+    }
+    for (let node of vertices) {
+      const [x, y] = node.value
+      if (x + 2 < boardSize && y + 1 < boardSize) node.addAdjacent(findVertex(x + 2, y + 1))
+      if (x + 1 < boardSize && y + 2 < boardSize) node.addAdjacent(findVertex(x + 1, y + 2))
+      if (x - 2 >= 0 && y + 1 < boardSize) node.addAdjacent(findVertex(x - 2, y + 1))
+      if (x - 1 >= 0 && y + 2 < boardSize) node.addAdjacent(findVertex(x - 1, y + 2))
+      if (x + 2 < boardSize && y - 1 >= 0) node.addAdjacent(findVertex(x + 2, y - 1))
+      if (x + 1 < boardSize && y - 2 >= 0) node.addAdjacent(findVertex(x + 1, y - 2))
+      if (x - 2 >= 0 && y - 1 >= 0) node.addAdjacent(findVertex(x - 2, y - 1))
+      if (x - 1 >= 0 && y - 2 >= 0) node.addAdjacent(findVertex(x - 1, y - 2))
+    }
+  }
+
+  buildGraph()
 }
